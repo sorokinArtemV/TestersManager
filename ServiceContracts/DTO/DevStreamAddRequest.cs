f62917c1c05e343c3190158d6e16f25bb5ebe0cd
Artem Sorokin <karanthir8@gmail.com>
2024-03-06 05:40:00 +0500

    add DevStreamAddResponse DTO

--- ServiceContracts/DTO/DevStreamAddRequest.cs
+++ ServiceContracts/DTO/DevStreamAddRequest.cs
@@ -2,6 +2,9 @@ using Entities;
 
 namespace ServiceContracts.DTO;
 
+/// <summary>
+/// DTO to add a new DevStream
+/// </summary>
 public class DevStreamAddRequest
 {
     public string? DevStreamName { get; set; }
---
+++ ServiceContracts/DTO/DevStreamResponse.cs
@@ -0,0 +1,25 @@
+using Entities;
+
+namespace ServiceContracts.DTO;
+
+/// <summary>
+/// DTO class to use as a return type of DevStreamService methods
+/// </summary>
+public class DevStreamResponse
+{
+    public Guid DevStreamId { get; set; }
+
+    public string? DevStreamName { get; set; }
+}
+
+public static class DevStreamExtensions
+{
+    public static DevStreamResponse ToDevStreamResponse(this DevStream devStream)
+    {
+        return new DevStreamResponse
+        {
+            DevStreamId = devStream.Id,
+            DevStreamName = devStream.DevStreamName
+        };
+    }
+}
