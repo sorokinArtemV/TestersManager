a9b9242c4e3b7a246a2c8daf8e13c9c9d09777d0
Artem Sorokin <karanthir8@gmail.com>
2024-03-14 06:34:52 +0500

    finish create form styling

--- TestersViewer/wwwroot/script.js
+++ TestersViewer/wwwroot/script.js
@@ -1,5 +1,5 @@
 'use strict'
 
 window.onload = function () {
-    document.querySelector('.button--secondary').scrollIntoView({behavior: 'smooth', block: 'start'});
+    document.querySelector('.container-form-create').scrollIntoView({behavior: 'smooth'});
 };
