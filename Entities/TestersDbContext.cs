1b296b57563875c7f2217d4f4d0a7d89abfa1026
Artem Sorokin <karanthir8@gmail.com>
2024-03-16 10:18:38 +0500

    add seed data to db

--- Entities/TestersDbContext.cs
+++ Entities/TestersDbContext.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 
 namespace Entities;
@@ -10,4 +11,28 @@ public class TestersDbContext : DbContext
 
     public DbSet<DevStream> DevStreams { get; set; }
     public DbSet<Tester> Testers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DevStream>().ToTable("DevStreams");
+        modelBuilder.Entity<Tester>().ToTable("Testers");
+        
+        // Seed data
+       var streamsJson = File.ReadAllText("streams.json");
+       var devStreams = JsonSerializer.Deserialize<List<DevStream>>(streamsJson);
+
+       foreach (var stream in devStreams!)
+       {
+           modelBuilder.Entity<DevStream>().HasData(stream);
+       }
+       
+       var testersJson = File.ReadAllText("testers.json");
+       var testers = JsonSerializer.Deserialize<List<DevStream>>(testersJson);
+
+       foreach (var tester in testers!)
+       {
+           modelBuilder.Entity<DevStream>().HasData(tester);
+       }
+
+    }
 }
