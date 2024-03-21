1ef6b4759f5a1aac35096c2125267fa408014cef
Artem Sorokin <karanthir8@gmail.com>
2024-03-21 07:51:04 +0500

    add logger config

--- TestersViewer/Program.cs
+++ TestersViewer/Program.cs
@@ -7,6 +7,8 @@ using Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Logging.ClearProviders().AddConsole().AddDebug();
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicatonDbContext>(options =>
@@ -15,12 +17,20 @@ builder.Services.AddDbContext<ApplicatonDbContext>(options =>
 });
 builder.Services.AddScoped<IDevStreamsService, DevStreamsService>();
 builder.Services.AddScoped<ITestersService, TestersService>();
+
 builder.Services.AddScoped<IDevStreamsRepository, DevStreamsRepository>();
 builder.Services.AddScoped<ITestersRepository, TestersRepository>();
 
 var app = builder.Build();
 
 if (builder.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+app.Logger.LogDebug("debug-message");
+app.Logger.LogInformation("info-message");
+app.Logger.LogWarning("warning-message");
+app.Logger.LogError("error-message");
+app.Logger.LogCritical("critical-message");
+
 
 app.UseStaticFiles();
 app.UseRouting();
