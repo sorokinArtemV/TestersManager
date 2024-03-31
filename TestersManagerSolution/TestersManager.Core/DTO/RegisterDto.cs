7c8219fea0121e8a5be54f27d52efb98e0338323
Artem Sorokin <karanthir8@gmail.com>
2024-03-31 12:23:07 +0500

    add register view

--- TestersManagerSolution/TestersManager.Core/DTO/RegisterDto.cs
+++ TestersManagerSolution/TestersManager.Core/DTO/RegisterDto.cs
@@ -6,20 +6,20 @@ public class RegisterDto
 {
     [Required(ErrorMessage = "Tester name is required")]
     public string TesterName { get; set; }
-    
+
     [Required(ErrorMessage = "Email is required")]
     [EmailAddress(ErrorMessage = "Email is not valid")]
     public string Email { get; set; }
 
-    [Required(ErrorMessage = "Phone number is required")]
-    [RegularExpression("^[0-9]*$", ErrorMessage = "Phone number can contain only numbers")]
-    [DataType(DataType.PhoneNumber)]
-    public string PhoneNumber { get; set; }
-    
+    // [Required(ErrorMessage = "Phone number is required")]
+    // [RegularExpression("^[0-9]*$", ErrorMessage = "Phone number can contain only numbers")]
+    // [DataType(DataType.PhoneNumber)]
+    // public string PhoneNumber { get; set; }
+
     [Required(ErrorMessage = "Password is required")]
     [DataType(DataType.Password)]
     public string Password { get; set; }
-    
+
     [Required(ErrorMessage = "Confirm password is required")]
     [DataType(DataType.Password)]
     public string ConfirmPassword { get; set; }
