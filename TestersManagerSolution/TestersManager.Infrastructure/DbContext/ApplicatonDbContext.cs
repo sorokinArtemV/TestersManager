2f35aaba6a802ab91a802b21762c74e8a2e15942
Artem Sorokin <karanthir8@gmail.com>
2024-03-31 08:01:02 +0500

    move Infrastructure level to new solution

---
+++ TestersManagerSolution/TestersManager.Infrastructure/DbContext/ApplicatonDbContext.cs
@@ -0,0 +1,46 @@
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using TestersManager.Core.Domain.Entities;
+
+namespace TestersManager.Infrastructure.DbContext;
+
+public class ApplicatonDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public ApplicatonDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public virtual DbSet<DevStream> DevStreams { get; set; }
+    public virtual DbSet<Tester> Testers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DevStream>().ToTable("DevStreams");
+        modelBuilder.Entity<Tester>().ToTable("Testers");
+
+        // Seed data
+        var streamsJson = File.ReadAllText("wwwroot/streams.json");
+        var devStreams = JsonSerializer.Deserialize<List<DevStream>>(streamsJson);
+
+        foreach (var stream in devStreams!) modelBuilder.Entity<DevStream>().HasData(stream);
+
+        var testersJson = File.ReadAllText("wwwroot/testers.json");
+        var testers = JsonSerializer.Deserialize<List<Tester>>(testersJson);
+
+        foreach (var tester in testers!) modelBuilder.Entity<Tester>().HasData(tester);
+
+        // fluent api
+        modelBuilder.Entity<Tester>().Property(x => x.MonthsOfWorkExperience)
+            .HasColumnName("WorksFor")
+            .HasColumnType("INTEGER")
+            .HasDefaultValue(1);
+
+        // table relations
+        // modelBuilder.Entity<Tester>(entity =>
+        // {
+        //     entity.HasOne<DevStream>(s => s.DevStream)
+        //         .WithMany(t => t.Testers)
+        //         .HasForeignKey(t => t.DevStreamId);
+        // });
+    }
+}
---
+++ TestersManagerSolution/TestersManager.Infrastructure/Repositories/DevStreamsRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestersManager.Core.Domain.Entities;
+using TestersManager.Core.Domain.RepositoryContracts;
+using TestersManager.Infrastructure.DbContext;
+
+namespace TestersManager.Infrastructure.Repositories;
+
+public class DevStreamsRepository : IDevStreamsRepository
+{
+    private readonly ApplicatonDbContext _db;
+
+    public DevStreamsRepository(ApplicatonDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<DevStream> AddDevStream(DevStream devStream)
+    {
+        _db.DevStreams.Add(devStream);
+        await _db.SaveChangesAsync();
+
+        return devStream;
+    }
+
+    public async Task<List<DevStream>> GetAllDevStreams()
+    {
+        return await _db.DevStreams.ToListAsync();
+    }
+
+    public async Task<DevStream?> GetDevStreamById(Guid devStreamId)
+    {
+        var devStream = await _db.DevStreams.FirstOrDefaultAsync(x => x.DevStreamId == devStreamId);
+
+        return devStream;
+    }
+
+    public async Task<DevStream?> GetDevStreamByName(string devStreamName)
+    {
+        var devStream = await _db.DevStreams.FirstOrDefaultAsync(x => x.DevStreamName == devStreamName);
+
+        return devStream;
+    }
+}
